80a5600049fbdf4bab79309a292c459a75327ecb
Devrim Kafadar <108999645+devrimkafadar@users.noreply.github.com>
2023-01-06 19:46:26 +0300

    Add files via upload

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const connectDatabase = async () => {
+    console.log('.........CONNECTING........');
+    try {
+        await mongoose.connect(process.env.MONGO_URI).then(
+            console.log('DB CONNECTION IS SUCCESSFUL.')
+        );
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+module.exports = connectDatabase;
